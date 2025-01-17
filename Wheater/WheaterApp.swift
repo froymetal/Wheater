a1d20668fa34a8add7e2986d0d06742a3505e256
froymetal <froylan10@hotmail.com>
2025-01-17 10:37:49 -0600

    Initial Commit

---
+++ Wheater/WheaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheaterApp.swift
+//  Wheater
+//
+//  Created by Consultant on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct WheaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
